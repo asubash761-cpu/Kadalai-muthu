03e51506d4a129c0014ec583d44df70982b159be
asubash761-cpu <asubash761@gmail.com>
2026-03-15 15:23:56 +0530

    update

--- script.js
+++ script.js
@@ -1,154 +1,138 @@
-// show registered user
-window.onload=function(){
+// REGISTER
+function register() {
+  let name = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
 
-let name=localStorage.getItem("user");
+  if(name=="" || pass=="") {
+    alert("Enter username and password");
+    return;
+  }
 
-if(name){
-document.getElementById("myname").innerHTML=name+" (You)";
+  let users = JSON.parse(localStorage.getItem("users")) || {};
+
+  if(users[name]){
+    alert("Username already registered");
+    return;
+  }
+
+  users[name] = pass;
+  localStorage.setItem("users", JSON.stringify(users));
+
+  alert("Register successful! Please login.");
+  window.location.href = "index.html";
 }
 
+// LOGIN
+function login() {
+  let name = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+
+  let users = JSON.parse(localStorage.getItem("users")) || {};
+
+  if(users[name] && users[name]==pass){
+    localStorage.setItem("user", name);
+    window.location.href = "chat.html";
+  } else {
+    alert("Wrong username or password");
+  }
 }
 
+// SHOW USER ON CHAT
+window.onload = function() {
+  let name = localStorage.getItem("user");
+  let my = document.getElementById("myname");
+  if(my && name){
+    my.innerHTML = name + " (You)";
+  }
+}
 
-// send text message
+// SEND MESSAGE
 function sendMsg(){
+  let input = document.getElementById("msg");
+  let chat = document.getElementById("chatbox");
+  let name = localStorage.getItem("user") || "User";
 
-let input=document.getElementById("msg");
-let chat=document.getElementById("chatbox");
-
-let name=localStorage.getItem("user") || "User";
-
-if(input.value.trim()!=""){
-
-let div=document.createElement("div");
-div.className="msg";
-
-div.innerHTML="<b>"+name+"</b><br>"+input.value;
-
-chat.appendChild(div);
-
-chat.scrollTop=chat.scrollHeight;
-
-input.value="";
-
+  if(input.value.trim()!=""){
+    let div = document.createElement("div");
+    div.className="msg";
+    div.innerHTML="<b>"+name+"</b><br>"+input.value;
+    chat.appendChild(div);
+    input.value="";
+    chat.scrollTop=chat.scrollHeight;
+  }
 }
 
-}
-
-
-// upload image/file
+// UPLOAD FILE/IMAGE
 function uploadFile(){
+  let file = document.getElementById("file").files[0];
+  if(!file) return;
 
-let file=document.getElementById("file").files[0];
-if(!file) return;
+  let url = URL.createObjectURL(file);
+  let chat = document.getElementById("chatbox");
+  let name = localStorage.getItem("user") || "User";
+  let div = document.createElement("div");
+  div.className="msg";
+  div.innerHTML="<b>"+name+"</b><br>";
 
-let url=URL.createObjectURL(file);
+  if(file.type.startsWith("image")){
+    let img=document.createElement("img");
+    img.src=url;
+    div.appendChild(img);
+  }
 
-let chat=document.getElementById("chatbox");
-
-let name=localStorage.getItem("user") || "User";
-
-let div=document.createElement("div");
-div.className="msg";
-
-div.innerHTML="<b>"+name+"</b><br>";
-
-if(file.type.startsWith("image")){
-
-let img=document.createElement("img");
-img.src=url;
-
-div.appendChild(img);
-
+  let link = document.createElement("a");
+  link.href = url;
+  link.download = file.name;
+  link.innerText = "Download "+file.name;
+  div.appendChild(link);
+  chat.appendChild(div);
+  chat.scrollTop=chat.scrollHeight;
 }
 
-let link=document.createElement("a");
-link.href=url;
-link.download=file.name;
-link.innerText="Download "+file.name;
-
-div.appendChild(link);
-
-chat.appendChild(div);
-
-chat.scrollTop=chat.scrollHeight;
-
-}
-
-
-// voice recording
+// VOICE MESSAGE
 let recorder;
 let chunks=[];
 
 function startVoice(){
-
-navigator.mediaDevices.getUserMedia({audio:true})
-.then(stream=>{
-
-recorder=new MediaRecorder(stream);
-
-recorder.start();
-
-chunks=[];
-
-recorder.ondataavailable=e=>chunks.push(e.data);
-
-recorder.onstop=e=>{
-
-let blob=new Blob(chunks,{type:"audio/webm"});
-let url=URL.createObjectURL(blob);
-
-let chat=document.getElementById("chatbox");
-
-let name=localStorage.getItem("user") || "User";
-
-let div=document.createElement("div");
-div.className="msg";
-
-div.innerHTML="<b>"+name+"</b><br>";
-
-let audio=document.createElement("audio");
-audio.controls=true;
-audio.src=url;
-
-let link=document.createElement("a");
-link.href=url;
-link.download="voice.webm";
-link.innerText="Download voice";
-
-div.appendChild(audio);
-div.appendChild(link);
-
-chat.appendChild(div);
-
-chat.scrollTop=chat.scrollHeight;
-
-};
-
-setTimeout(()=>recorder.stop(),4000);
-
-})
-.catch(()=>alert("Allow microphone permission"));
-
+  navigator.mediaDevices.getUserMedia({audio:true})
+  .then(stream=>{
+    recorder = new MediaRecorder(stream);
+    recorder.start();
+    chunks=[];
+    recorder.ondataavailable=e=>chunks.push(e.data);
+    recorder.onstop=e=>{
+      let blob=new Blob(chunks,{type:"audio/webm"});
+      let url=URL.createObjectURL(blob);
+      let chat=document.getElementById("chatbox");
+      let name=localStorage.getItem("user") || "User";
+      let div=document.createElement("div");
+      div.className="msg";
+      div.innerHTML="<b>"+name+"</b><br>";
+      let audio=document.createElement("audio");
+      audio.controls=true;
+      audio.src=url;
+      div.appendChild(audio);
+      let link=document.createElement("a");
+      link.href=url;
+      link.download="voice.webm";
+      link.innerText="Download voice";
+      div.appendChild(link);
+      chat.appendChild(div);
+      chat.scrollTop=chat.scrollHeight;
+    };
+    setTimeout(()=>recorder.stop(),4000);
+  })
+  .catch(()=>alert("Allow microphone permission"));
 }
 
-
-// members toggle
+// MEMBERS TOGGLE
 function toggleMembers(){
-
-let m=document.getElementById("members");
-
-if(m.style.display=="block")
-m.style.display="none";
-else
-m.style.display="block";
-
+  let m=document.getElementById("members");
+  if(m.style.display=="block") m.style.display="none";
+  else m.style.display="block";
 }
 
-
-// voice call demo
+// VOICE CALL DEMO
 function voiceCall(){
-
-alert("Voice call needs WebRTC server");
-
+  alert("Voice call needs server");
 }
